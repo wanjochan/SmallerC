b9483f94a917ba2ea765640fc8659afa88d59191
wanjochan <wanjochan@gmail.com>
2018-09-16 18:02:31 +0800

    docker tcc build osx binary and test

---
+++ build_osx.sh
@@ -0,0 +1,7 @@
+#build with docker
+dkrun sh build_osx_bin.sh
+
+#test
+./test_c.osx
+
+# notes, upx not working yet
---
+++ build_osx_bin.sh
@@ -0,0 +1,4 @@
+./configure
+make
+make install
+smlrcc -macos test_c.c -o test_c.osx
